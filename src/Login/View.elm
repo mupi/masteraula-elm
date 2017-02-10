d6c0844b10efc49ef49c202a1f36864458b771ee
Ana Rute <anarutecvm@gmail.com>
2017-02-10 16:48:38 -0200

    Improve login view layout
    Remove sign up form - it will be a different view.

--- src/Login/View.elm
+++ src/Login/View.elm
@@ -8,6 +8,8 @@ import Material.Textfield as Textfield
 import Material.Button as Button
 import Material.Options as Options exposing (css)
 import Material.Grid exposing (grid, cell, size, offset, Device(..))
+import Material.Typography as Typo
+import Material.Color as Color
 
 
 -- import State exposing (..)
@@ -15,48 +17,53 @@ import Material.Grid exposing (grid, cell, size, offset, Device(..))
 
 view : Model -> Html Msg
 view model =
-    grid []
-        [ cell [ size All 6 ]
-            [ Html.h1 [] [ text "Sign-up" ]
+    div []
+        [ grid []
+            [ cell [ size All 6, offset All 3, size Phone 12 ]
+                [ Options.styled h1
+                    [ Typo.display1, Typo.center ]
+                    [ text "Entrar no PrePaula" ]
+                , div []
+                    [ Textfield.render Mdl
+                        [ 2 ]
+                        model.mdl
+                        [ Textfield.label "Usuário"
+                        , Textfield.text_
+                        , Textfield.floatingLabel
+                        , Textfield.value model.username
+                        , Options.onInput SetUsername
+                        ]
+                        []
+                    ]
+                , div []
+                    [ Textfield.render Mdl
+                        [ 3 ]
+                        model.mdl
+                        [ Textfield.label "Senha"
+                        , Textfield.floatingLabel
+                        , Textfield.password
+                        , Textfield.value model.password
+                        , Options.onInput SetPassword
+                        ]
+                        []
+                    ]
+                , div [ class "text-alert" ]
+                    [ text model.error ]
+                , div []
+                    [ Button.render Mdl
+                        [ 2 ]
+                        model.mdl
+                        [ Button.raised
+                        , Button.colored
+                        , Options.onClick Login
+                        ]
+                        [ text "Sign in" ]
+                    ]
+                ]
             ]
-        , cell [ size All 6 ]
-            [ Html.h1 [] [ text "Login Form" ]
-            , div []
-                [ Textfield.render Mdl
-                    [ 0 ]
-                    model.mdl
-                    [ Textfield.label "Usuário"
-                    , Textfield.text_
-                    , Textfield.floatingLabel
-                    , Textfield.value model.username
-                    , Options.onInput SetUsername
-                    ]
-                    []
-                ]
-            , div []
-                [ Textfield.render Mdl
-                    [ 1 ]
-                    model.mdl
-                    [ Textfield.label "Senha"
-                    , Textfield.floatingLabel
-                    , Textfield.password
-                    , Textfield.value model.password
-                    , Options.onInput SetPassword
-                    ]
-                    []
-                ]
-            , div [ class "text-alert" ]
-                [ text model.error ]
-            , div []
-                [ Button.render Mdl
-                    [ 0 ]
-                    model.mdl
-                    [ Button.raised
-                    , Button.colored
-                    , Options.onClick Login
-                    ]
-                    [ text "Sign in" ]
-                ]
-            , text (toString model)
+        , div [ class "banner-bg" ]
+            [ Options.styled h1
+                [ Typo.display1, Typo.center ]
+                [ text "Não possui login? Cadastre-se agora" ]
             ]
         ]
